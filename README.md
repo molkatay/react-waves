aa51053fd312b53b4ff3212a8dd0fb9127982f03
Molka Tayahi <tayahi.molka@gmail.com>
2019-03-11 14:02:21 +0000

    Adds site info

---
+++ README.md
@@ -0,0 +1,1 @@
+# Waves React App
---
+++ server/models/site.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+
+const siteSchema = mongoose.Schema({
+    featured:{
+        type: Array,
+        required: true,
+        default: []
+    },
+    siteInfo:{
+        type: Array,
+        required: true,
+        default: []
+    }
+});
+
+const Site = mongoose.model('Site',siteSchema);
+module.exports = { Site }
